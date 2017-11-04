b26778863885108103b090544dc6b70d09dd88d7
Mert Serezli <mertserezli@gmail.com>
2017-11-04 10:02:50 +0300

    postgresql gecisi yapildi

--- Bil372Project/src/dataAccess/ConnectionManager.java
+++ Bil372Project/src/dataAccess/ConnectionManager.java
@@ -8,16 +8,18 @@ public class ConnectionManager {
 
 	public Connection getConnection() {
 		try {
-			Class.forName("org.sqlite.JDBC");
+			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e2) {
 
 		}
 		Connection conn = null;
 		try {
 			// db parameters
-			String url = "jdbc:sqlite:" + this.getClass().getResource("/").getPath() + "database.db";
+			String url = "jdbc:postgresql://projectmanagementdb.c7iazxagbuhz.eu-central-1.rds.amazonaws.com/projectmanagementdb";
+			String username = "mertserezli";
+			String password = "55numara";
 			// create a connection to the database
-			conn = DriverManager.getConnection(url);
+			conn = DriverManager.getConnection(url, username, password);
 
 		} catch (SQLException e) {
 
