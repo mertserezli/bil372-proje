bf60cb0e652f1413b6c68484cf74979613a5e251
Sertan Özcan <sertan_821@hotmail.com>
2017-11-18 21:44:06 +0300

    Arama sayfası iyileştirildi, CompanyBean eklendi

---
+++ Bil372Project/src/models/CompanyBean.java
@@ -0,0 +1,32 @@
+package models;
+
+public class CompanyBean {
+	private String CompID;
+	private String Name;
+	private String Description;
+
+	public String getCompID() {
+		return CompID;
+	}
+
+	public void setCompID(String compID) {
+		CompID = compID;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+}
