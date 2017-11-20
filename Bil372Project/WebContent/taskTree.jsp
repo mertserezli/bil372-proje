3e736c067362531fb2e3003adb8de9b195fe76dc
Mert Serezli <mertserezli@gmail.com>
2017-11-20 17:55:12 +0300

    get projects employee list added

--- Bil372Project/WebContent/taskTree.jsp
+++ Bil372Project/WebContent/taskTree.jsp
@@ -1,17 +1,39 @@
 <%@ page language="java" 
 	contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"
+    import="models.UserBean"
     %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		 <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/tree.css" />
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<title>Task Tree</title>
+		<% UserBean currentUser = (UserBean)session.getAttribute("currentSessionUser");
+   			UserBean currentProfile=new UserBean();%>
 	</head>
 	<body>
+		<ul>
+		  <li><a href="profile.jsp?username=<%=currentUser.getUsername()%>"><%=currentUser.getUsername() %></a></li>
+		  <li><a href="search.jsp">Search</a></li>
+		  <li><a href="mytasks.jsp">My Tasks</a></li>
+		  <li><a href="mymessages.jsp">My Messages</a></li>
+		  <li><a href="index.jsp">Logout</a></li>
+		</ul>
 		<% String html = (String) request.getAttribute("tree"); %>
 		<%= html %>
+		<br>
+		
+		<br>
+		<form action="addEmployeeToTask" method="get"><!--TODO: burayi duzelt--> 
+			<label>task title</label>
+			<input type="text" name="title">
+			<label>employee</label>
+			<input type="text" name="description">
+			
+			<input type="submit" value="Create">  
+		</form>
 	</body>
 	<style type="text/css">
 
--- Bil372Project/src/dataAccess/ProjectDAO.java
+++ Bil372Project/src/dataAccess/ProjectDAO.java
@@ -10,110 +10,156 @@ import java.util.Arrays;
 import java.util.Calendar;
 
 import models.ProjectBean;
+import models.UserBean;
 
 public class ProjectDAO {
 	static Connection currentCon = null;
 	static ResultSet rs = null;
 	static PreparedStatement ps = null;
-	static ConnectionManager connect=null;
-	
-	public static ProjectBean getProject(ProjectBean project){
-		int pid=project.getPid();
-		String query="select * from project where pid=?";
-		try{
-			connect=new ConnectionManager();
-			currentCon=connect.getConnection();
-			ps=currentCon.prepareStatement(query);
-			ps.setInt(1,pid);
+	static ConnectionManager connect = null;
+
+	public static ProjectBean getProject(ProjectBean project) {
+		int pid = project.getPid();
+		String query = "select * from project where pid=?";
+		try {
+			connect = new ConnectionManager();
+			currentCon = connect.getConnection();
+			ps = currentCon.prepareStatement(query);
+			ps.setInt(1, pid);
 			rs = ps.executeQuery();
 			boolean more = rs.next();
-			if(more){
+			if (more) {
 				project.setCreation_Date(rs.getDate("creation_date"));
 				project.setDescription(rs.getString("description"));
-				//project.setState(rs.get);
+				// project.setState(rs.get);
 				project.setTitle(rs.getString("title"));
 				project.setVotenum(rs.getInt("votenum"));
-				try{
-					project.setTags((String[])rs.getArray("tags").getArray());
-					
-				}
-				catch(Exception e){
+				try {
+					project.setTags((String[]) rs.getArray("tags").getArray());
+
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				try{
-					project.setMeeting_dates((Date[])rs.getArray("meeting_dates").getArray());
-				}
-				catch(Exception e){
+				try {
+					project.setMeeting_dates((Date[]) rs.getArray("meeting_dates").getArray());
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
-			}
-			else{
+
+			} else {
 				project.setTitle("Project Not Found");
 			}
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return project;
 	}
-	
-	public static ProjectBean setNewMeeting(ProjectBean project,String date){
+
+	public static ProjectBean setNewMeeting(ProjectBean project, String date) {
 		int i;
-		Date[] meetings=null;
-		String query="update project set meeting_dates=? where pid=?";
-		String day=date.substring(0,date.indexOf("."));
-		date=date.substring(date.indexOf(".")+1);
-		String month=date.substring(0,date.indexOf("."));
-		date=date.substring(date.indexOf(".")+1);
-		String year=date;
-		Date newMeeting = new Date(Integer.parseInt(year)-1900,Integer.parseInt(month)-1,Integer.parseInt(day));
-		
-		if(project.getMeeting_dates()!=null){
-			meetings=new Date[project.getMeeting_dates().length+1];
-			
-			for(i=0;i<project.getMeeting_dates().length;i++){
-				meetings[i]=project.getMeeting_dates()[i];
+		Date[] meetings = null;
+		String query = "update project set meeting_dates=? where pid=?";
+		String day = date.substring(0, date.indexOf("."));
+		date = date.substring(date.indexOf(".") + 1);
+		String month = date.substring(0, date.indexOf("."));
+		date = date.substring(date.indexOf(".") + 1);
+		String year = date;
+		Date newMeeting = new Date(Integer.parseInt(year) - 1900, Integer.parseInt(month) - 1, Integer.parseInt(day));
+
+		if (project.getMeeting_dates() != null) {
+			meetings = new Date[project.getMeeting_dates().length + 1];
+
+			for (i = 0; i < project.getMeeting_dates().length; i++) {
+				meetings[i] = project.getMeeting_dates()[i];
 			}
-			meetings[i]=newMeeting;
+			meetings[i] = newMeeting;
+		} else {
+			meetings = new Date[1];
+			meetings[0] = newMeeting;
 		}
-		else{
-			meetings=new Date[1];
-			meetings[0]=newMeeting;
-		}
-		
-		try{
-			connect=new ConnectionManager();
-			currentCon=connect.getConnection();
-			ps=currentCon.prepareStatement(query);
-			ps.setArray(1, currentCon.createArrayOf("DATE",meetings));
+
+		try {
+			connect = new ConnectionManager();
+			currentCon = connect.getConnection();
+			ps = currentCon.prepareStatement(query);
+			ps.setArray(1, currentCon.createArrayOf("DATE", meetings));
 			ps.setInt(2, project.getPid());
 			ps.executeUpdate();
 			project.setMeeting_dates(meetings);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return project;
 	}
-	public static boolean createProject(ProjectBean project){
-		String insertQuery="insert into project (title,description,tags,creation_date) values (?,?,?,?)";
-		try{
-			connect=new ConnectionManager();
-			currentCon=connect.getConnection();
-			ps=currentCon.prepareStatement(insertQuery);
+
+	public static boolean createProject(ProjectBean project) {
+		String insertQuery = "insert into project (title,description,tags,creation_date) values (?,?,?,?)";
+		try {
+			connect = new ConnectionManager();
+			currentCon = connect.getConnection();
+			ps = currentCon.prepareStatement(insertQuery);
 			ps.setString(1, project.getTitle());
 			ps.setString(2, project.getDescription());
-			Array tags=currentCon.createArrayOf("TEXT", project.getTags());
-			ps.setArray(3,tags);
-			ps.setDate(4,new java.sql.Date(Calendar.getInstance().getTime().getTime()));
+			Array tags = currentCon.createArrayOf("TEXT", project.getTags());
+			ps.setArray(3, tags);
+			ps.setDate(4, new java.sql.Date(Calendar.getInstance().getTime().getTime()));
 			ps.executeUpdate();
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
-	
+
+	public static ArrayList<UserBean> getEmployees(ProjectBean project) {
+		int pid = project.getPid();
+		ArrayList<UserBean> result = new ArrayList<UserBean>();
+		String query = "select * from work_emp_pro w, employee e where w.pid=? AND w.username=e.username";
+		try {
+			connect = new ConnectionManager();
+			currentCon = connect.getConnection();
+			ps = currentCon.prepareStatement(query);
+			ps.setInt(1, pid);
+			rs = ps.executeQuery();
+			boolean more = rs.next();
+			while (rs.next()) {
+				UserBean employee = new UserBean();
+
+				employee.setUserName(rs.getString("username"));
+				employee.setEmail(rs.getString("email"));
+				employee.setFirstName(rs.getString("firstname"));
+				employee.setJobTitle(rs.getString("jobtitle"));
+				employee.setLastName(rs.getString("lastname"));
+				employee.setMiddleName(rs.getString("middlename"));
+
+				result.add(employee);
+			}
+		} catch (Exception ex) {
+			System.out.println("Failed: An Exception has occurred! " + ex);
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+				}
+				rs = null;
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (Exception e) {
+				}
+				ps = null;
+			}
+			if (currentCon != null) {
+				try {
+					currentCon.close();
+				} catch (Exception e) {
+				}
+				currentCon = null;
+			}
+		}
+		return result;
+	}
+
 }
