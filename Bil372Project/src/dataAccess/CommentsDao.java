8178a66996ac797d1f462ac95dd5efd569bd16e1
Cem Ozen <cemsozens@hotmail.com>
2017-11-20 21:45:05 +0300

    comment kısmı eklendi

---
+++ Bil372Project/src/dataAccess/CommentsDao.java
@@ -0,0 +1,32 @@
+package dataAccess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import models.CommentBean;
+import models.ProjectBean;
+import models.UserBean;
+
+public class CommentsDao {
+	static Connection currentCon = null;
+	static PreparedStatement ps = null;
+	static ConnectionManager connect = null;
+	
+	public static boolean addComment(ProjectBean project,UserBean user,CommentBean comment) {
+		String query="insert into project_comments (username,content,pid) values (?,?,?)";
+		try {
+			connect = new ConnectionManager();
+			currentCon = connect.getConnection();
+			ps = currentCon.prepareStatement(query);
+			ps.setString(1, user.getUsername());
+			ps.setString(2, comment.getContent());
+			ps.setInt(3, project.getPid());
+			ps.executeUpdate();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+}
